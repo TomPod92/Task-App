598a5f23c1b0b0474e92c3d304bcbfa654cf320a
tpodsiadlik <tpodsiadlik@FP-LSC0166>
2023-01-12 17:08:16 +0100

    Create shell components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 // import Button from 'components/Button';
+import { Task } from 'types';
+import { dummyTasksData } from 'dummy-data';
+import { TaskForm } from 'components/TaskForm/TaskForm';
+import { TasksColumn } from 'components/TasksColumn/TasksColumn';
+import { Loader } from 'components/Loader/Loader';
 import './App.scss';
 
 const App = () => {
-  const [show, setShow] = useState(false);
-  const [count, setCount] = useState(0);
-  const toggleShow = () => {
-    setShow((p) => !p);
-  };
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasksLoading, setTasksLoading] = useState(true);
+  const [selectedTask, setSelectedTask] = useState<Task>();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTasks(dummyTasksData);
+      setTasksLoading(false);
+    }, 3000);
+  }, []);
 
   return (
     <div className="app">
-      <header className="App-header">Header</header>
-      {show && <div data-testid="test">I am here</div>}
-      {/* <Button /> */}
-      <div data-testid="count">{count}</div>
-      <button onClick={toggleShow}>Click</button>
-      <button onClick={() => setCount((p) => p + 1)}>Increment</button>
+      {tasksLoading && <Loader />}
+      <div className="tasks-table-container">
+        <div className="tasks-table">
+          <TasksColumn />
+          <TasksColumn />
+          <TasksColumn />
+        </div>
+      </div>
+      <TaskForm />
     </div>
   );
 };
---
+++ src/components/TasksColumn/TasksColumn.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const TasksColumn = () => {
+  return <div>TasksColumn</div>;
+};
