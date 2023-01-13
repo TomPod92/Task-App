43581c05c2766ffcf1ae6481e7619ee79fa8ecab
tpodsiadlik <tpodsiadlik@FP-LSC0166>
2023-01-13 22:02:22 +0100

    Create Loader component

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import AutorenewIcon from '@mui/icons-material/Autorenew';
+import './loader.scss';
+interface Props {
+  isOpen: boolean;
+}
 
-export const Loader = () => {
-  return <div>Loader</div>;
+export const Loader = ({ isOpen }: Props) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="loader">
+      <div className="loader-icon">
+        <AutorenewIcon />
+      </div>
+    </div>
+  );
 };
